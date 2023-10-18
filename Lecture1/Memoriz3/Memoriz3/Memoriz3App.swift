df20b78b162d854b15ce0274367b2ccb34424aca
favade <fav.adek0ya@gmail.com>
2023-10-18 18:01:49 -0400

    Initial Commit CardView isFaceUp

---
+++ Lecture1/Memoriz3/Memoriz3/Memoriz3App.swift
@@ -0,0 +1,17 @@
+//
+//  Memoriz3App.swift
+//  Memoriz3
+//
+//  Created by Favour on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Memoriz3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
