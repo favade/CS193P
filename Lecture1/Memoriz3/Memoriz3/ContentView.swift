a03ef3bdb9795c964330b188b24f21bc8192b76f
favade <fav.adek0ya@gmail.com>
2023-10-18 18:32:22 -0400

    Toggable isFaceUp

--- Lecture1/Memoriz3/Memoriz3/ContentView.swift
+++ Lecture1/Memoriz3/Memoriz3/ContentView.swift
@@ -9,15 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        HStack(content: {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        })
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+struct CardView: View {
+   @State var isFaceUp = false
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("🍆")
+                    .font(.largeTitle)
+            } else {
+                base
+            }
+           
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
