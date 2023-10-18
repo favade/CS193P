e7c589be009f0ed6afbf99bc865dca9add816199
favade <fav.adek0ya@gmail.com>
2023-10-18 18:48:36 -0400

    Array-ified the cards with ForEach

--- Lecture1/Memoriz3/Memoriz3/ContentView.swift
+++ Lecture1/Memoriz3/Memoriz3/ContentView.swift
@@ -8,67 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["👹", "🎃", "🕷️", "👻", "👻"]
     var body: some View {
-        HStack(content: {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
-        })
+        HStack {
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+        }
         .foregroundColor(.orange)
         .padding()
     }
 }
-
-struct CardView: View {
-   @State var isFaceUp = false
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text("🍆")
-                    .font(.largeTitle)
-            } else {
-                base
+    
+    struct CardView: View {
+        let content: String
+        @State var isFaceUp = true
+        var body: some View {
+            ZStack {
+                let base = RoundedRectangle(cornerRadius: 12)
+                if isFaceUp {
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth: 2)
+                    Text(content)
+                        .font(.largeTitle)
+                } else {
+                    base
+                }
+                
+            }.onTapGesture {
+                isFaceUp.toggle()
             }
-           
-        }.onTapGesture {
-            isFaceUp.toggle()
         }
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Preview {
-    ContentView()
-}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #Preview {
+        ContentView()
+    }
+    
